71072696ac7053afea1df8a42238234151838b7b
duston04 <darshankatkar44@gmail.com>
2023-03-03 22:28:07 +0530

    CORS filter added

--- backend/src/main/java/com/healthcareapp/backend/controller/PatientController.java
+++ backend/src/main/java/com/healthcareapp/backend/controller/PatientController.java
@@ -3,12 +3,10 @@ package com.healthcareapp.backend.controller;
 import com.healthcareapp.backend.entities.Patient;
 import com.healthcareapp.backend.patientservice.PatientServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class PatientController {
 
     @Autowired
--- backend/src/main/java/com/healthcareapp/backend/controller/PendingQueueController.java
+++ backend/src/main/java/com/healthcareapp/backend/controller/PendingQueueController.java
@@ -8,16 +8,12 @@ import com.healthcareapp.backend.patientservice.PatientServices;
 import com.healthcareapp.backend.patientservice.PendingQueueServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 //import com.healthcareapp.backend.entities.PendingQueue;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +24,7 @@ import java.util.Optional;
 
 
 @RestController
+@CrossOrigin
 public class PendingQueueController {
 
     @Autowired
