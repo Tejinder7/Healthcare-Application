89608ed516818785b979e4d22d65d7103f011ecc
duston04 <darshankatkar44@gmail.com>
2023-03-31 12:01:53 +0530

    Forgot password End-point added

--- backend/src/main/java/com/healthcareapp/backend/Controller/LoginController.java
+++ backend/src/main/java/com/healthcareapp/backend/Controller/LoginController.java
@@ -2,10 +2,10 @@ package com.healthcareapp.backend.Controller;
 
 import com.healthcareapp.backend.Model.Authorization;
 import com.healthcareapp.backend.Service.AuthorizationService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -19,5 +19,15 @@ public class LoginController {
     @PostMapping("/login")
     public boolean loginUser(@RequestBody Authorization authorization){
         return authorizationService.loginAuthorization(authorization);
+    }
+
+    @PostMapping("/forgotPassword/{email}")
+    public List<Object> forgotPassword(@PathVariable String email){
+        List<Object> list = new ArrayList<>();
+        Boolean boolValue = true;
+        list.add(boolValue);
+        String msg = "Please check email to change the password";
+        list.add(msg);
+        return list;
     }
 }
