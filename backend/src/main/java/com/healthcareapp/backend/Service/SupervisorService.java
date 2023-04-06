f66cb9df4bfefd1b1fb24f1b88b73e76517fcf25
Gaurang <gaurang1235@live.com>
2023-04-06 20:28:57 +0530

    unassigned Patients API added

--- backend/src/main/java/com/healthcareapp/backend/Service/SupervisorService.java
+++ backend/src/main/java/com/healthcareapp/backend/Service/SupervisorService.java
@@ -1,8 +1,15 @@
 package com.healthcareapp.backend.Service;
 
+import com.healthcareapp.backend.Model.FollowUp;
+import com.healthcareapp.backend.Model.Hospital;
+import com.healthcareapp.backend.Model.Patient;
 import com.healthcareapp.backend.Model.Supervisor;
 import com.healthcareapp.backend.Repository.SupervisorRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class SupervisorService {
     SupervisorRepository supervisorRepository;
@@ -19,6 +26,27 @@ public class SupervisorService {
         catch (Exception e){
             throw new RuntimeException();
         }
+    }
+
+    public List<Patient> unAssignedPatients(int supId){
+        Supervisor supervisor = supervisorRepository.findSupervisorBySupId(supId);
+
+        if(supervisor==null)
+        {
+            throw new RuntimeException();
+        }
+
+        List<Hospital> hospitalList = supervisor.getHospitalList();
+
+        List<FollowUp> followUpList = new ArrayList<>();
+
+        hospitalList.forEach(hospital -> {followUpList.addAll(hospital.getFollowUpList());});
+
+        List<Patient> patientList = new ArrayList<>();
+
+        followUpList.forEach(followUp -> {if(followUp.getPatientId()==null) patientList.add(followUp.getPatientId());});
+
+        return patientList;
     }
 }
 
