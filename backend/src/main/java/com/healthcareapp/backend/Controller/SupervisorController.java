602b11a8881232eb0a17a568961d5b7a68c62f75
Gaurang <gaurang1235@live.com>
2023-04-06 20:27:12 +0530

    Doc, FW, FD userid&password added, Hospital supervisor auto addition enabled

--- backend/src/main/java/com/healthcareapp/backend/Controller/SupervisorController.java
+++ backend/src/main/java/com/healthcareapp/backend/Controller/SupervisorController.java
@@ -1,13 +1,13 @@
 package com.healthcareapp.backend.Controller;
 
+import com.healthcareapp.backend.Model.Patient;
 import com.healthcareapp.backend.Model.Supervisor;
 import com.healthcareapp.backend.Service.SupervisorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -29,5 +29,17 @@ public class SupervisorController {
             return ResponseEntity.status(500).build();
         }
         return ResponseEntity.of(Optional.of(supervisor1));
+    }
+
+    @PostMapping("/unassignedPatients/{supId}")
+    public ResponseEntity<List<Patient>> unassignedPatients(@PathVariable int supId){
+        List<Patient> patientList = new ArrayList<>();
+
+        try{
+            patientList = supervisorService.unAssignedPatients(supId);
+        }catch (Exception e){
+            return ResponseEntity.status(500).build();
+        }
+        return ResponseEntity.of(Optional.of(patientList));
     }
 }
