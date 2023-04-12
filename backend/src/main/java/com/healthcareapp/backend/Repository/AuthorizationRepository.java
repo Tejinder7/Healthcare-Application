66e549324ffdd48e6d99442a89b3467f3b971aed
Tejinder7 <t.saggu99@gmail.com>
2023-04-12 11:19:28 +0530

    Added userId validation to admin registration

--- backend/src/main/java/com/healthcareapp/backend/Repository/AuthorizationRepository.java
+++ backend/src/main/java/com/healthcareapp/backend/Repository/AuthorizationRepository.java
@@ -3,8 +3,10 @@ package com.healthcareapp.backend.Repository;
 import com.healthcareapp.backend.Model.Authorization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorizationRepository extends JpaRepository<Authorization, Integer> {
-    public Authorization findByUserIdAndPasswordAndUserType(String userId, String password, String userType);
+import java.util.Optional;
 
-    public Authorization findAuthorizationByUserId(String userId);
+public interface AuthorizationRepository extends JpaRepository<Authorization, Integer> {
+    public Optional<Authorization> findByUserIdAndPasswordAndUserType(String userId, String password, String userType);
+
+    public Optional<Authorization> findByUserId(String userId);
 }
